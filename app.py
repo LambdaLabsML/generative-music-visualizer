f5a1e6eec05864626a6ca5d95e7d1c3ceff83fa4
Jeremy Hummel <jh@lambdal.com>
2022-12-12 13:00:25 -0500

    Updates markdown

--- app.py
+++ app.py
@@ -33,41 +33,41 @@ network_choices = [
 ]
 
 description = \
-    """
-    Generate visualizations on an input audio file using [StyleGAN3](https://nvlabs.github.io/stylegan3/) (Karras, Tero, et al. "Alias-free generative adversarial networks." Advances in Neural Information Processing Systems 34 (2021): 852-863.).
-    Inspired by [Deep Music Visualizer](https://github.com/msieg/deep-music-visualizer), which used BigGAN (Brock et al., 2018) 
-    Developed by Jeremy Hummel at [Lambda](https://lambdalabs.com/)
-    """
+"""
+Generate visualizations on an input audio file using [StyleGAN3](https://nvlabs.github.io/stylegan3/) (Karras, Tero, et al. "Alias-free generative adversarial networks." Advances in Neural Information Processing Systems 34 (2021): 852-863.).
+Inspired by [Deep Music Visualizer](https://github.com/msieg/deep-music-visualizer), which used BigGAN (Brock et al., 2018) 
+Developed by Jeremy Hummel at [Lambda](https://lambdalabs.com/)
+"""
 
 article = \
-    """
-    ## How does this work?
-    The audio is transformed to a spectral representation by using Short-time Fourier transform (STFT). [librosa]() 
-    Starting with an initial noise vector, we perform a random walk, adjusting the length of each step with the power gradient.
-    This pushes the noise vector to move around more when the sound changes.
-    
-    ## Parameter info:
-    *Network*: various pre-trained models from NVIDIA, "afhqv2" is animals, "ffhq" is faces, "metfaces" is artwork.
-    
-    *Truncation*: controls how far the noise vector can be from the origin. `0.7` will generate more realistic, but less diverse samples,
-    while `1.2` will can yield more interesting but less realistic images.
-    
-    *Tempo Sensitivity*: controls the how the size of each step scales with the audio features
-    
-    *Jitter*: prevents the same exact noise vectors from cycling repetitively, if set to `0`, the images will repeat during
-    repetitive parts of the audio
-    
-    *Frame Length*: controls the number of audio frames per video frame in the output. 
-    If you want a higher frame rate for visualizing very rapid music, lower the frame length. 
-    If you want a lower frame rate (which will complete the job faster), raise the frame length
-    
-    *Max Duration*: controls the max length of the visualization, in seconds. Use a shorter value here to get output
-    more quickly, especially for testing different combinations of parameters.
-    
-    Media sources:
-    [Maple Leaf Rag - Scott Joplin (1916, public domain)](https://commons.wikimedia.org/wiki/File:Maple_leaf_rag_-_played_by_Scott_Joplin_1916_V2.ogg)
-    [Moonlight Sonata Opus 27. no 2. - movement 3 - Ludwig van Beethoven, played by Muriel Nguyen Xuan (2008, CC BY-SA 3.0)](https://commons.wikimedia.org/wiki/File:Muriel-Nguyen-Xuan-Beethovens-Moonlight-Sonata-mvt-3.oga)
-    """
+"""
+## How does this work?
+The audio is transformed to a spectral representation by using Short-time Fourier transform (STFT). [librosa]() 
+Starting with an initial noise vector, we perform a random walk, adjusting the length of each step with the power gradient.
+This pushes the noise vector to move around more when the sound changes.
+
+## Parameter info:
+*Network*: various pre-trained models from NVIDIA, "afhqv2" is animals, "ffhq" is faces, "metfaces" is artwork.
+
+*Truncation*: controls how far the noise vector can be from the origin. `0.7` will generate more realistic, but less diverse samples,
+while `1.2` will can yield more interesting but less realistic images.
+
+*Tempo Sensitivity*: controls the how the size of each step scales with the audio features
+
+*Jitter*: prevents the same exact noise vectors from cycling repetitively, if set to `0`, the images will repeat during
+repetitive parts of the audio
+
+*Frame Length*: controls the number of audio frames per video frame in the output. 
+If you want a higher frame rate for visualizing very rapid music, lower the frame length. 
+If you want a lower frame rate (which will complete the job faster), raise the frame length
+
+*Max Duration*: controls the max length of the visualization, in seconds. Use a shorter value here to get output
+more quickly, especially for testing different combinations of parameters.
+"""
+# Media sources:
+# [Maple Leaf Rag - Scott Joplin (1916, public domain)](https://commons.wikimedia.org/wiki/File:Maple_leaf_rag_-_played_by_Scott_Joplin_1916_V2.ogg)
+# [Moonlight Sonata Opus 27. no 2. - movement 3 - Ludwig van Beethoven, played by Muriel Nguyen Xuan (2008, CC BY-SA 3.0)](https://commons.wikimedia.org/wiki/File:Muriel-Nguyen-Xuan-Beethovens-Moonlight-Sonata-mvt-3.oga)
+# """
 
 examples = [
     ["examples/Maple_leaf_rag_-_played_by_Scott_Joplin_1916_V2.ogg", network_choices[0], 1.0, 0.25, 0.5, 512, 600],
